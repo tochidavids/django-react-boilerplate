6ceac53888a003365603a46a30ab0350b59e6f9f
Tochi Davids <tochidavids18@gmail.com>
2023-07-21 19:48:01 +0100

    added homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,20 @@
 export default function Home() {
-    return <h1>Homepage</h1>
+	return (
+		<main className="homepage">
+			<div className="left">
+				<h1>
+					Trello brings all your tasks, teammates, and tools together
+				</h1>
+				<p>
+					Keep everything in the same place — even if your team isn’t.
+				</p>
+			</div>
+			<div className="right">
+				<img
+					src="https://images.ctfassets.net/rz1oowkt5gyp/75rDABL8fyMtNLlUAtBxrg/c5e145977a86c41c47e17c69410c64f7/TrelloUICollage_4x.png?w=2280&fm=webp"
+					alt=""
+				/>
+			</div>
+		</main>
+	);
 }
