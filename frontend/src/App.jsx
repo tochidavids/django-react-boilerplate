3dcf79d96411207cbd59d9579ccb8fc318e8651c
Tochi Davids <tochidavids18@gmail.com>
2023-07-21 23:43:29 +0100

    added plans page as route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,21 @@ import Features from "./pages/Features";
 import Pricing from "./pages/Pricing";
 import Resources from "./pages/Resources";
 import Solutions from "./pages/Solutions";
+import Plans from "./pages/Plans";
 
 function App() {
 	return (
 		<BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<Home />} />
-          <Route path="/features" element={<Features/>} />
-          <Route path="/pricing" element={<Pricing/>} />
-          <Route path="/resources" element={<Resources/>} />
-          <Route path="/solutions" element={<Solutions/>} />
-        </Route>
-      </Routes>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path="/features" element={<Features />} />
+					<Route path="/pricing" element={<Pricing />} />
+					<Route path="/resources" element={<Resources />} />
+					<Route path="/solutions" element={<Solutions />} />
+					<Route path="/plans" element={<Plans />} />
+				</Route>
+			</Routes>
 		</BrowserRouter>
 	);
 }
