b8c6e03b79db755a79bb26c4df4fc47e07332cb4
Tochi Davids <tochidavids18@gmail.com>
2023-07-21 23:58:17 +0100

    moved react app into respective django app

--- trello/settings.py
+++ trello/settings.py
@@ -57,7 +57,10 @@ ROOT_URLCONF = 'trello.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend', 'build')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'landing' 'frontend', 'build'),
+            os.path.join(BASE_DIR, 'landing', 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'landing', 'frontend', 'build', 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
