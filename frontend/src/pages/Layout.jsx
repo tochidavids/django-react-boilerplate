a13b1353a7d660f39194ffb006869ad7f91cf56a
Tochi Davids <tochidavids18@gmail.com>
2023-07-21 22:28:15 +0100

    header fully responsive

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,21 +1,53 @@
+import { useEffect, useState, useRef } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 export default function Layout() {
+	const [showMenu, setShowMenu] = useState(false);
+	const headerRef = useRef();
+
+	useEffect(() => {
+		if (showMenu) {
+			document.body.style.overflow = "hidden";
+			headerRef.current.className = "show-menu";
+		} else {
+			document.body.style.overflow = "unset";
+			headerRef.current.className = "";
+		}
+	}, [showMenu]);
+
 	return (
 		<div className="page">
-			<header>
+			<header ref={headerRef}>
 				<section className="left">
-					<Link to="/">
-						<div className="img-container logo">
+					<Link to="/" className="home-link">
+						<div className="img-container">
 							<img
 								src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8c/Trello_logo.svg/1280px-Trello_logo.svg.png"
 								alt=""
 							/>
 						</div>
 					</Link>
-					<nav>
+					{window.innerWidth < 975 ? (
+						!showMenu ? (
+							<i
+								className="fa-solid fa-bars"
+								onClick={() => setShowMenu(true)}
+							></i>
+						) : (
+							<i
+								className="fa-solid fa-xmark"
+								onClick={() => setShowMenu(false)}
+							></i>
+						)
+					) : (
+						""
+					)}
+
+					<nav className="menu">
 						<li>
-							<Link to="/features">features</Link>
+							<Link to="/features" className="first">
+								features
+							</Link>
 						</li>
 						<li>
 							<Link to="/solutions">solutions</Link>
@@ -31,7 +63,8 @@ export default function Layout() {
 						</li>
 					</nav>
 				</section>
-				<div className="sign-in">
+
+				<div className="sign-in menu">
 					<a className="login" href="/login">
 						Log in
 					</a>
