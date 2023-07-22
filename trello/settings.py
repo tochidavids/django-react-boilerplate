c0b6fc5f2ebac9ea25b5f5a4e05e3afa283dffb3
Tochi Davids <tochidavids18@gmail.com>
2023-07-23 00:25:09 +0100

    added google sign in options on templates

--- trello/settings.py
+++ trello/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites', 
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -89,11 +94,11 @@ DATABASES = {
 
         'USER': 'postgres',
 
-        'PASSWORD': 'QxHxW5ipAjPjPyEPvuUl',
+        'PASSWORD': 'dr9KYyZAupCFHbemZfQN',
 
-        'HOST': 'containers-us-west-205.railway.app',
+        'HOST': 'containers-us-west-54.railway.app',
 
-        'PORT': '6682',
+        'PORT': '7285',
     }
 }
 
@@ -146,3 +151,29 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+
+# Additional configuration settings
+SOCIALACCOUNT_QUERY_EMAIL = True
+ACCOUNT_LOGOUT_ON_GET= True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_EMAIL_REQUIRED = True
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
