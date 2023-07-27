b9a1d9c43d3f713086efba4d0fce227f8691ae37
Tochi Davids <tochidavids18@gmail.com>
2023-07-27 15:07:12 +0100

    added nav bar features and icons

--- main/frontend/src/main.jsx
+++ main/frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/output.css'
+import './styles/input.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- main/frontend/src/pages/Layout.jsx
+++ main/frontend/src/pages/Layout.jsx
@@ -13,26 +13,37 @@ function Layout() {
 						/>
 					</Link>
 					<div className="relative">
-						Workspaces
-						<div className="drop-down">
-                            {/* <Link to='/'>
+						Workspaces{" "}
+						<i className="fa-solid fa-chevron-down ml-4"></i>
+						<div className="">
+							{/* <Link to='/'>
                             </Link> */}
-                        </div>
+						</div>
 					</div>
 					<div className="relative">
-						Recent
-						<div className="drop-down">
-                            {/* <Link to='/'>
+						Recent <i className="fa-solid fa-chevron-down ml-4"></i>
+						<div className="">
+							{/* <Link to='/'>
                             </Link> */}
-                        </div>
+						</div>
 					</div>
 					<div className="relative">
-                        Starred
-						<div className="drop-down">
-                            {/* <Link to='/'>
+						Starred{" "}
+						<i className="fa-solid fa-chevron-down ml-4"></i>
+						<div className="">
+							{/* <Link to='/'>
                             </Link> */}
-                        </div>
+						</div>
 					</div>
+				</nav>
+				<nav className="flex justify-center items-center gap-4">
+					<div className="flex items-center justify-start gap-4 px-4">
+						<i className="fa-solid fa-magnifying-glass"></i>
+						<input type="text" className="border-0 outline-0" />
+					</div>
+					<i className="fa-regular fa-bell "></i>
+					<i className="fa-solid fa-circle-question"></i>
+					<i className="fa-solid fa-circle-half-stroke"></i>
 				</nav>
 			</header>
 			<Outlet />
@@ -40,4 +51,4 @@ function Layout() {
 	);
 }
 
-export default Layout
+export default Layout;
