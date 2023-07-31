b9b63c251e3a9b24872935c8790afe1d72e2117c
Tochi Davids <tochidavids18@gmail.com>
2023-07-31 15:14:44 +0100

    tailwind --> scss

--- main/frontend/src/main.jsx
+++ main/frontend/src/main.jsx
@@ -1,18 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./styles/output.css";
-import "./styles/input.css";
+import "./styles/styles.css";
 
-const root = document.getElementById("root");
-
-document.documentElement.classList.add('dark')
-
-// export const toggleDarkMode = () => {
-//   root.classList.toggle('dark')
-// }
-
-ReactDOM.createRoot(root).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
