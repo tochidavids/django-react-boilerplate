bf24cf1c1993f78dbbbcf8bf8e3e40f380c62c9b
Tochi Davids <tochidavids18@gmail.com>
2023-08-02 22:22:45 +0100

    fully styled navbar

---
+++ main/frontend/src/components/Sidebar.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Sidebar() {
+	const [showWorkspaceNav, setShowWorkspaceNav] = useState(true);
+
+	return (
+		<nav className="sidebar">
+			<Link to="/" className="nav-link boards focus">
+				<i className="fa-brands fa-trello"></i>
+				Boards
+			</Link>
+			<div className="line"></div>
+			<div className="add">
+				<div className="">Workspaces</div>
+				<i className="fa-solid fa-plus"></i>
+			</div>
+			<section className="workspace">
+				<div
+					className="title"
+					onClick={() => setShowWorkspaceNav(old => !old)}
+				>
+					<h1>Trello Workspace</h1>
+					<i
+						className={`fa-solid fa-chevron-${
+							showWorkspaceNav ? "up" : "down"
+						}`}
+					></i>
+				</div>
+				{showWorkspaceNav ? (
+					<div className="links">
+						<Link to="/boards" className="nav-link">
+							<i className="fa-brands fa-trello"></i>
+							Boards
+						</Link>
+						<Link to="/highlights" className="nav-link">
+							<i className="fa-solid fa-heart"></i>
+							Highlights
+						</Link>
+						<Link to="/members" className="nav-link">
+							<i className="fa-solid fa-user"></i>
+							Members
+						</Link>
+						<Link to="/" className="nav-link">
+							<i className="fa-solid fa-gear"></i>
+							Settings
+						</Link>
+					</div>
+				) : (
+					""
+				)}
+			</section>
+		</nav>
+	);
+}
