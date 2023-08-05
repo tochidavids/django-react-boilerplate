f242dd64732b63200dc7de59e91cf91a53d408cb
Tochi Davids <tochidavids18@gmail.com>
2023-08-05 21:22:15 +0100

    added starred boards view

--- main/frontend/src/pages/board/Boards.jsx
+++ main/frontend/src/pages/board/Boards.jsx
@@ -4,6 +4,7 @@ import { getDatabaseData } from "../../main";
 
 export default function Boards({ current }) {
 	const [boards, setBoards] = useState([]);
+	const [starredBoards, setStarredBoards] = useState([]);
 	const [workspaceView, setWorkspaceView] = useState();
 
 	useEffect(() => {
@@ -14,18 +15,23 @@ export default function Boards({ current }) {
 			if (typeof workspaceView !== "undefined") {
 				let boards = await getDatabaseData("boards");
 				const currentUser = await getDatabaseData("current-user");
-				if (boards)
+				let starred
+				if (boards) {
 					boards = boards.filter(
 						board => board.creator == currentUser[1].id,
 					);
 
-				if (workspaceView) {
-					if (boards)
+					if (workspaceView) {
 						boards = boards.filter(
 							board => board.workspace == current.id,
 						);
+					}
+					starred = boards.filter(board =>
+						Boolean(board.starred),
+					);
 				}
 				setBoards(boards);
+				setStarredBoards(starred);
 			}
 		};
 		getData();
@@ -34,6 +40,22 @@ export default function Boards({ current }) {
 
 	return (
 		<main className="boards">
+			{starredBoards ? (
+				<>
+					<h1>Starred Boards</h1>
+					<section className="all-boards">
+						{starredBoards &&
+							starredBoards.map((board, index) => (
+								<Link to="/board-name" key={index}>
+									<img src={board.background} alt="" />
+									<h1>{board.name}</h1>
+								</Link>
+							))}
+					</section>
+				</>
+			) : (
+				""
+			)}
 			<h1>{workspaceView ? "" : "Your"} Boards</h1>
 			<section className="all-boards">
 				{boards &&
@@ -43,6 +65,7 @@ export default function Boards({ current }) {
 							<h1>{board.name}</h1>
 						</Link>
 					))}
+				{boards ? '': <p>Your boards appear here</p>}
 			</section>
 		</main>
 	);
